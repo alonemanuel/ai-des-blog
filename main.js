dfecc0ccfe596fc1107529a4f3dea069a2186006
alonemanuel <alon@alonemanuel.com>
2023-04-18 21:27:47 +0300

    Done

---
+++ main.js
@@ -0,0 +1,12 @@
+main();
+// var canvasDiv = document.getElementById(`myCanvas`);
+// var width = canvasDiv.offsetWidth;
+
+function main() {
+    createBody();
+}
+
+function createBody() {
+    console.debug(`creating body`);
+    // let mainPanel = document.body.appendChild(document.createElement(`div`));
+}
